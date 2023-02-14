09ae4b5f755ae9fd18233727a9e8081e0fb6547a
Senjuti De <sde@redhat.com>
2023-02-14 12:24:54 +0530

    go.sum file updated :wq

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main(){
-	
+import (
+	"flag"
+	"log"
+	"context"
+    "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+var (
+ kuberconfig = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
+)
+
+
+func main() {
+ flag.Parse()
+
+
+ cfg, err := clientcmd.BuildConfigFromFlags("", *kuberconfig)
+ if err != nil {
+	log.Fatalf("Error building kubeconfig: %v", err)
+ }
+
+
+ clientset, err := kubernetes.NewForConfig(cfg)
+ if err != nil {
+    log.Fatalf("Error building example clientset: %v", err)
+ }
+ namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to list namespaces: %v", err)
+	}
+
+	log.Printf("Number of namespaces: %d", len(namespaces.Items))
 }
